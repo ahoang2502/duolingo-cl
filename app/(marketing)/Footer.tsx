b3c4bb4be0489bdbe06046493359de4d90cf33e4
ahoang2502 <athoang252@gmail.com>
2024-03-07 03:25:00 +1100

    marketing-page: content + footer

--- app/(marketing)/Footer.tsx
+++ app/(marketing)/Footer.tsx
@@ -1,9 +1,63 @@
+import Image from "next/image";
 import React from "react";
+
+import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
 	return (
 		<footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-			Footer
+			<div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+				<Button className="w-full" size="lg" variant="ghost">
+					<Image
+						src="/hr.svg"
+						alt="croatian"
+						height={32}
+						width={32}
+						className="mr-4 rounded-md"
+					/>
+					Croatian
+				</Button>
+				<Button className="w-full" size="lg" variant="ghost">
+					<Image
+						src="/es.svg"
+						alt="spanish"
+						height={32}
+						width={32}
+						className="mr-4 rounded-md"
+					/>
+					Spanish
+				</Button>
+				<Button className="w-full" size="lg" variant="ghost">
+					<Image
+						src="/fr.svg"
+						alt="french"
+						height={32}
+						width={32}
+						className="mr-4 rounded-md"
+					/>
+					French
+				</Button>
+				<Button className="w-full" size="lg" variant="ghost">
+					<Image
+						src="/it.svg"
+						alt="italian"
+						height={32}
+						width={32}
+						className="mr-4 rounded-md"
+					/>
+					Italian
+				</Button>
+				<Button className="w-full" size="lg" variant="ghost">
+					<Image
+						src="/jp.svg"
+						alt="japanese"
+						height={32}
+						width={32}
+						className="mr-4 rounded-md"
+					/>
+					Japanese
+				</Button>
+			</div>
 		</footer>
 	);
 };
