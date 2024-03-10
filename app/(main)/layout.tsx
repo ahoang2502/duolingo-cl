cbf60600c815248a7607515de619c88913150ec0
ahoang2502 <athoang252@gmail.com>
2024-03-11 02:34:40 +1100

    feat: learn-page wrappers

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,11 +10,11 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
 	return (
 		<>
-            <MobileHeader />
+			<MobileHeader />
 			<Sidebar className="hidden lg:flex" />
 
-			<main className="pl-[256px] h-full pt-[50px] lg:pt-0">
-				<div className="h-full">{children}</div>
+			<main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+				<div className="h-full max-w-[1056px] mx-auto pt-6">{children}</div>
 			</main>
 		</>
 	);
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import { StickyWrapper } from "@/components/StickyWrapper";
+import { FeedWrapper } from "@/components/FeedWrapper";
+import { Header } from "./Header";
+import { UserProgress } from "@/components/UserProgress";
+
 const LearnPage = () => {
-	return <div>LearnPage</div>;
+	return (
+		<div className="flex flex-row-reverse gap-[48px] px-6">
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={{ title: "spanish", imageSrc: "/next.svg" }}
+					hearts={5}
+					points={100}
+					hasActiveSubscription={false}
+				/>
+			</StickyWrapper>
+
+			<FeedWrapper>
+				<Header title="spanish" />
+			</FeedWrapper>
+		</div>
+	);
 };
 
 export default LearnPage;
