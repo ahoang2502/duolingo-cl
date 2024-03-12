38d311d1ae9ddb742d6cbf1ed64951a0c0936c7f
ahoang2502 <athoang252@gmail.com>
2024-03-13 04:10:14 +1100

    feat: user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,18 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
 import { StickyWrapper } from "@/components/StickyWrapper";
 import { FeedWrapper } from "@/components/FeedWrapper";
 import { Header } from "./Header";
 import { UserProgress } from "@/components/UserProgress";
+import { getUserProgress } from "@/db/queries";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+	const userProgressData = getUserProgress();
+	const [userProgress] = await Promise.all([userProgressData]);
+
+	if (!userProgress || !userProgress.activeCourse) redirect("/courses");
+
 	return (
 		<div className="flex flex-row-reverse gap-[48px] px-6">
 			<StickyWrapper>
