9f0df3cb756b0a3ccdd43f18028e6fd7f155cb17
ahoang2502 <athoang252@gmail.com>
2024-03-16 22:00:03 +1100

    feat: course progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -5,19 +5,31 @@ import { StickyWrapper } from "@/components/StickyWrapper";
 import { FeedWrapper } from "@/components/FeedWrapper";
 import { Header } from "./Header";
 import { UserProgress } from "@/components/UserProgress";
-import { getUnits, getUserProgress } from "@/db/queries";
+import {
+	getCourseProgress,
+	getLessonPercentage,
+	getUnits,
+	getUserProgress,
+} from "@/db/queries";
 import { Unit } from "./Unit";
 
 const LearnPage = async () => {
 	const userProgressData = getUserProgress();
 	const unitsData = getUnits();
+	const courseProgressData = getCourseProgress();
+	const lessonPercentageData = getLessonPercentage();
 
-	const [userProgress, units] = await Promise.all([
-		userProgressData,
-		unitsData,
-	]);
+	const [userProgress, units, courseProgress, lessonPercentage] =
+		await Promise.all([
+			userProgressData,
+			unitsData,
+			courseProgressData,
+			lessonPercentageData,
+		]);
 
 	if (!userProgress || !userProgress.activeCourse) redirect("/courses");
+
+	if (!courseProgress) redirect("/courses");
 
 	return (
 		<div className="flex flex-row-reverse gap-[48px] px-6">
@@ -41,8 +53,8 @@ const LearnPage = async () => {
 							description={unit.description}
 							title={unit.title}
 							lessons={unit.lessons}
-							activeLesson={undefined}
-							activeLessonPercentage={0}
+							activeLesson={courseProgress.activeLesson}
+							activeLessonPercentage={lessonPercentage}
 						/>
 					</div>
 				))}
