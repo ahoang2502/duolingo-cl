6462168d8e47e84288991095c0934e8ef5943d81
ahoang2502 <athoang252@gmail.com>
2024-07-01 15:03:31 +1000

    feat: admin: units

--- app/admin/_ components/App.tsx
+++ app/admin/_ components/App.tsx
@@ -4,8 +4,11 @@ import simpleRestProvider from "ra-data-simple-rest";
 import { Admin, Resource } from "react-admin";
 
 import { CourseCreate } from "../course/Create";
-import { CourseList } from "../course/List";
 import { CourseEdit } from "../course/Edit";
+import { CourseList } from "../course/List";
+import { UnitCreate } from "../unit/Create";
+import { UnitEdit } from "../unit/Edit";
+import { UnitList } from "../unit/List";
 
 const dataProvider = simpleRestProvider("/api");
 
@@ -17,8 +20,15 @@ const App = () => {
 				name="courses"
 				recordRepresentation="title"
 				list={CourseList}
-                create={CourseCreate}
+				create={CourseCreate}
 				edit={CourseEdit}
+			/>
+			<Resource
+				name="units"
+				recordRepresentation="title"
+				list={UnitList}
+				create={UnitCreate}
+				edit={UnitEdit}
 			/>
 		</Admin>
 	);
